1e3d59c6c1981dfb310f340c013a2071fedbf5fe
Yazmin Tinoco <ytip30@gmail.com>
2021-04-14 02:35:55 -0500

    The current day is displayed at the top of the calendar

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -1,21 +1,20 @@
 //the current day is displayed at the top of the calendar
-var currentDay  = moment();
+var currentDay = moment();
+console.log('day--->', currentDay)
+var currentHour = moment().format('HH');
+console.log('hour---->', currentHour)
 $("#currentDay").text(currentDay.format('dddd[,]MMMM Do'));
 
-var hoursPerDay = 8;
-var time = [];
 
-function timeOneDay(){
-    var formattedTime;
-    for(i =0; i < hoursPerDay+1 ; i++){ //fill in all of the hours
-        formattedTime = (moment().subtract(i-2, "hours")).format("hA");  //give the time in format X AM/PM
-        time.unshift(formattedTime);  //add to beginning of array
-    }                                //do this for all 24 hours
-}
-timeOneDay();
-console.log(time);
-time.forEach(element => {
-    console.log(element)
-});
 
-$("#first").text(time[0]);
+
+
+
+
+
+
+
+
+
+
+
